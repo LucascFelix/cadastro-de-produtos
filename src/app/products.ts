b139ecdb5afb0432ade9e1881808a1b614a8188a
Lucas Felix <lucacfelix@gmail.com>
2024-03-21 20:47:02 -0300

    :construction: criando component,array e bind

---
+++ src/app/products.ts
@@ -0,0 +1,11 @@
+export interface Products {
+    id: number,
+    name: string,
+    price: number,
+    quantity: number
+
+    
+}
+export interface description {
+    description: string
+}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Products, description } from '../products';
 
 @Component({
   selector: 'app-products',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  products: Products[] = [
+
+    { id: 1, name: "lápis", price: 2.25, quantity: 200 },
+    { id: 2, name: "Borracha", price: 1.99, quantity: 50 },
+    { id: 3, name: "Cola Branca", price: 3.99, quantity: 80 }
+
+  ];
+
+  description: description[] = [
+
+    { description: "Lápis de Cor Sextavado Ecolápis 24 Cores Faber-Castell cx 1 un" }
+  ]
 }
